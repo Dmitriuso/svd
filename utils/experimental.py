2827edf606749df7b4ed82220e161c1dcbc77369
Dmitri Osipov <dmitri.osipov@lexistems.com>
2021-09-30 11:15:06 +0300

    iterating through tensor

--- utils/experimental.py
+++ utils/experimental.py
@@ -1,18 +1,28 @@
-from torch import ones, zeros, diag
+from torch import ones, zeros, diag, svd_lowrank
 from torch.linalg import svd
 import torch.nn as nn
+from svd_compression import svd_compress
 
 # define a matrix
-A = ones(128, 17, 256)
+A = zeros(128, 17, 256)
 print(A.shape)
 
-U, s, VT = svd(A)
+U, s, VT = svd_lowrank(A, q=17)
 
 # create m x n Sigma matrix
-Sigma = zeros(A.shape[0], A.shape[1], A.shape[2])
+Sigma = zeros(A.shape[0], A.shape[1])
 
 # populate Sigma with n x n diagonal matrix
-# Sigma[:A.shape[1], :A.shape[1]] = diag(s)
+Sigma[:A.shape[0], :A.shape[0]] = diag(s)
+
+svdlr = svd_lowrank(A, q=2)
+
+for i in A:
+    print(i.shape)
+    B = i.cpu().data.numpy()
+    compressed = svd_compress(B)
+    print(compressed.shape)
+
 
 if __name__ == '__main__':
     print(f'the input matrix:\n {A.shape}')
@@ -20,6 +30,6 @@ if __name__ == '__main__':
     print(f'the SVD matrices:\nU: {U.shape}\ns:{s.shape}\nVT: {VT.shape}')
     print('*'*50)
     print(f'∑ matrix populated with diag matrix after SVD:\n {Sigma.shape}')
-
-
+    print('*'*50)
+    print(f'the SVD of a low-rank matrix A:\n {[i.shape for i in svd_lowrank(A)]}')
 
